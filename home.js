74ce4eae8a53302f41453b770291eab6b073c161
devsteffanny <steffanny.sabrina@hotmail.com>
2022-06-23 14:37:39 -0300

    Nova home finalizada

--- home.js
+++ home.js
@@ -1,3 +1,9 @@
 function mostraNome(nome){
     return nome
 }
+
+function mostraSobrenome(sobreNome){
+    return nome
+}
+
+//Finalizado o sistema da home
--- login.js
+++ login.js
@@ -2,3 +2,11 @@
 //Trabalhando no login
 
 //Finalizando o sistema de login
+function login(name, email){
+    return 'Bem vindo';
+}
+
+
+
+//Sistema de login completo
+//retornamos o nome do usuario quando logado
